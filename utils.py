21844001e90913c7b22cdac4bb0f278411372575
Ajaila0069 <christian.sungmin.park@gmail.com>
2021-03-23 11:09:43 -0700

    utils

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import math
+import re
 
 def noisify(set, diff=0.5):
     noisy = set + np.random.normal(0, diff, size=set.shape)
@@ -42,3 +43,22 @@ def divide_and_shuffle(set1, set2, size=0.5):
         newsmallx = [small[0] for small in smallnew]
         newsmally = [small[1] for small in smallnew]
         return np.array(newlargex), np.array(newsmallx), np.array(newlargey), np.array(newsmally)
+
+def substrings(main, size, step = 1):
+    main = re.sub("\n+", " ", main)
+    main = re.sub("[^a-zA-z 1-9]", "", main)
+    corpus = main.split()
+    length = len(corpus)
+    slices = []
+    #print(((length - (size-1))//step))
+    for i in range(0, ((length - (size-1))//step)):
+        idx = i * step
+        try:
+            slices.append(corpus[idx:idx+size])
+        except Exception as e:
+            print(e)
+    return slices
+
+
+if __name__ == "__main__":
+    print(substrings(open("chugjug.txt", "r").read(), 3, step=2))
